1f15173173ffceda5263576d4da61976cb6e0012
root <root@iZ25e6ys67nZ.(none)>
2016-06-25 17:43:14 +0800

    add data-connection

--- mysite/app1/models.py
+++ mysite/app1/models.py
@@ -1,5 +1,8 @@
+# coding:utf-8
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Obj(models.Model):
+    name = models.CharField(u'名称', max_length=256)
+    location = models.CharField(u'位置', max_length=256)
--- mysite/app1/views.py
+++ mysite/app1/views.py
@@ -1,9 +1,12 @@
 #coding:utf-8
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Obj
 
 def home(request):
-    return render(request, 'home.html')
+    rowRange = range(5)
+    Objs = Obj.objects.all()
+    return render(request, 'home.html',{'obj':Objs})
 
 def index(request):
     return HttpResponse(u"欢迎光临 Cindy的收纳窗！")
