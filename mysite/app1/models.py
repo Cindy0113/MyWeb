6b2a83c26cb3e9aa6d3bc1df1a4c803cb91d4d0e
root <root@iZ254vctagrZ.(none)>
2016-06-27 00:34:38 +0800

    add attributes of data

--- mysite/app1/models.py
+++ mysite/app1/models.py
@@ -1,8 +1,12 @@
 # coding:utf-8
 from __future__ import unicode_literals
-
+from datetime import datetime
 from django.db import models
 
 class Obj(models.Model):
     name = models.CharField(u'名称', max_length=256)
+    type = models.CharField(u'型号', max_length=256, default='no type')
+    category = models.CharField(u'类别', max_length=256, default='no category')
     location = models.CharField(u'位置', max_length=256)
+    buy_date = models.DateTimeField(u'购买时间', editable=True, default=datetime.now())
+    buy_address = models.TextField(u'购买地点', max_length=2048, default='no address')
